0561ab170279f2226d2ede482ec5f5c945ddf8b9
DiegoDiazDev <luisdiazdhz@gmail.com>
2022-03-28 10:30:03 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module:{
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|png|jpeg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name:'[name].[ext]',
+              outputPath: 'static/',
+              useRelativePath: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    })
+  ]
+}
